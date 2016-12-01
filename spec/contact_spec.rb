dadb4e75218c511c62e58ef13cdcc9c40c789c50
Hal Fairless <hbfairless@gmail.com>
2016-12-01 10:04:45 -0800

    adds corrections and revised directory and file structures

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,28 +1,29 @@
 require('rspec')
 require('contact')
-require('phone')
-require('address')
+# require('phone')
+# require('address')
 
 describe(Contact) do
   before() do
     Contact.clear()
   end
 
-  describe('#name') do
-    it('returns the name of the contact') do
-      test_contact = Contact.new({:name => 'Audrie Talbot',:birth_month => 07})
+  describe('#first_name') do
+    it('returns the first name of the contact') do
+      test_contact = Contact.new("Hal", "Fairless")
       test_contact.save()
-      expect(test_contact.name()).to(eq('Audrie Talbot'))
+      expect(test_contact.first_name()).to(eq('Hal'))
     end
   end
 
-  describe('#birth_month') do
-    it('returns the birth month of a contact') do
-      test_contact = Contact.new({:name => 'Audrie Talbot', :birth_month => 07})
+  describe('#last_name') do
+    it('returns the last name of the contact') do
+      test_contact = Contact.new("Hal", "Fairless")
       test_contact.save()
-      expect(test_contact.birth_month()).to(eq(07))
+      expect(test_contact.last_name()).to(eq('Fairless'))
     end
   end
+
 
   describe('.all') do
     it('is empty at first') do
@@ -30,31 +31,4 @@ describe(Contact) do
     end
   end
 
-  describe('.find') do
-    it('returns a contact by its name') do
-      test_contact = Contact.new({:name => 'Audrie Talbot', :birth_month => 07})
-      test_contact.save()
-      test_contact_2 = Contact.new({:name => 'Jeanette Fairless', :birth_month => 06})
-      test_contact_2.save()
-      expect(Contact.find(test_contact.name())).to(eq(test_contact))
-    end
-  end
-
-  describe('#add_phone') do
-    it('adds a new phone to a contact') do
-      test_contact = Contact.new({:name => 'Audrie Talbot', :birth_month => 07})
-      test_phone = Phone.new({:type => 'cell', :number => '111111111'})
-      test_contact.add_phone(test_phone)
-      expect(test_contact.phones()).to(eq([test_phone]))
-    end
-  end
-
-  describe('#add_address') do
-    it('adds a new address to a contact') do
-      test_contact = Contact.new({:name => 'Audrie Talbot', :birth_month => 07})
-      test_address = Address.new({:address_line => '11640 NW Stone Mountain Lane', :city => 'Portland', :state => 'OR', :zip => '97229'})
-      test_contact.add_address(test_address)
-      expect(test_contact.addresses()).to(eq([test_address]))
-    end
-  end
-end
+nd
