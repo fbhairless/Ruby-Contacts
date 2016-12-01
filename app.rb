32dc594fee1f2e364be315cb40b408ea33a9bc10
Hal Fairless <hbfairless@gmail.com>
2016-12-01 14:44:52 -0800

    routing and testing modifications

--- app.rb
+++ app.rb
@@ -5,23 +5,23 @@ require('./lib/contact')
 # # require('./lib/phone')
 # # require('./lib/address')
 #
-# get('/') do
-#   erb(:index)
-# end
-#
-# get('/contacts') do
-#   @contacts = Contact.all()
-#   erb(:contacts)
-# end
-#
-# get('/contacts/new') do
-#   erb(:contact_form)
-# end
-#
-# post('/contacts') do
-#   name = params.fetch('name')
-#   birth_month = params.fetch('birth_month')
-#   Contact.new({:name => name, :birth_month => birth_month}).save()
-#   @contacts = Contact.all()
-#   erb(:contacts)
-# end
+get('/') do
+  erb(:index)
+end
+
+get('/contacts') do
+  @contacts = Contact.all()
+  erb(:contacts)
+end
+
+get('/contacts/new') do
+  erb(:contact_form)
+end
+
+post('/contacts') do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  Contact.new("Hal", "Fairless").save()
+  @contacts = Contact.all()
+  erb(:contacts)
+end
